a4a72bd71b8935ee9443e8e52a4d21f528b542c5
Gabriel Santos <gabi.santos.pu@gmail.com>
2022-12-03 23:33:32 -0300

    Creation of the menu for the mobile version

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+$('.btnOpen').click(function(){
+    $('.menuLateral').toggleClass('open');
+})
+
+$('.btnClose').click(function(){
+    $('.menuLateral').toggleClass('close');
+})
+
+$('.btnMove').click(function(){
+    $('.btn').toggleClass('gira')
+})
