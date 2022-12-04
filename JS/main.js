4ec88b604aeeef9e2c5c12ca3fb0d53bae655002
Gabriel Santos <gabi.santos.pu@gmail.com>
2022-12-04 08:56:25 -0300

    Update for hidden mobile menu

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,10 @@
 $('.btnOpen').click(function(){
     $('.menuLateral').toggleClass('open');
+      
 })
+
 
 $('.btnClose').click(function(){
     $('.menuLateral').toggleClass('close');
 })
 
-$('.btnMove').click(function(){
-    $('.btn').toggleClass('gira')
-})
